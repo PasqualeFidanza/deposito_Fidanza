0937e4a9994c98d8a2db2f40a417b05eedb94204
PasqualeFidanza <Pasquale.Fidanza@it.ey.com>
2025-08-19 11:11:09 +0200

    Training del modello, confusion matrix e report

--- 19Agosto/Esercizio1/dt.py
+++ 19Agosto/Esercizio1/dt.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 # Lettura del dataset
 df = pd.read_csv("AEP_hourly.csv", parse_dates=['Datetime'])
 
@@ -15,11 +17,18 @@ df['target'] = (df['AEP_MW'] > df['AEP_MW'].mean()).astype(int)
 # Creazione di training e test set
 X = df[['year', 'month', 'day', 'hour']]
 y = df['target']
+print(df.head())
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=42)
 
 # Creazione del modello Decision Tree
 dt = DecisionTreeClassifier(max_depth=5, random_state=42)
 
+dt.fit(X_train, y_train)
+y_pred = dt.predict(X_test)
 
+# Print della confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
 
-print(df.head())
+# print del report
+print(classification_report(y_pred, y_test))
