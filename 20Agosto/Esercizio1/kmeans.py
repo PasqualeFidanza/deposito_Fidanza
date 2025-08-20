cd524841823e638eac7946628cd042eb5db3697f
PasqualeFidanza <Pasquale.Fidanza@it.ey.com>
2025-08-20 11:47:03 +0200

    Creazione del modello e analisi dei dati

---
+++ 20Agosto/Esercizio1/kmeans.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import numpy as np
+df = pd.read_csv('Esercizio1\Mall_Customers.csv')
+
+X = df[['Annual Income (k$)', 'Spending Score (1-100)']]
+X_values = X.values
+
+# Creazione del modello K-Means, fit e predict
+km = KMeans(n_clusters=5, random_state=0)
+labels = km.fit_predict(X_values)
+
+plt.figure(figsize=(6, 6))
+plt.scatter(X_values[:, 0], X_values[:, 1], c=labels, cmap='viridis')
+plt.show()
+
+centroids = km.cluster_centers_
+print(centroids)
+
+# Calcolo del cluster ad alto potenziale e plot del relativo centroide
+scores = centroids[:, 0] + centroids[:, 1]
+best_cluster = np.argmax(scores)
+print(best_cluster)
+
+best_customers = X_values[labels == best_cluster]
+
+plt.figure(figsize=(6, 6))
+plt.scatter(X_values[:, 0], X_values[:, 1], c=labels, cmap='viridis')
+plt.scatter(
+    centroids[best_cluster, 0],
+    centroids[best_cluster, 1],
+    c='red',
+    s=250,
+    marker='X',
+    edgecolor='k',
+    label='Best Cluster Centroid'
+)
+plt.show()
